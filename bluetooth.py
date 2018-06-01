30c3b504054b6200062c93d999ab6bfac9b5489d
George Castle <voxlap@gmail.com>
2018-06-01 01:23:56 +0000

    i hate this

--- bluetooth.py
+++ bluetooth.py
@@ -1,42 +1,40 @@
 import subprocess
 import os
 devices = {'D4:63:C6:3B:B9:BD': 0}
-print(subprocess.Popen("cmdout=$(hcitool rssi " + list(devices.keys())[0] + ")", shell=True, stdout=subprocess.PIPE).stdout.read())
 
 btconnected=0
 btcurrent=-1
 counter=0
-notconnected="0"
-connected="1"
+notconnected=0
+connected=1
 rssi=-1
 
 #Command loop:
 while (True):
+    cmdout= os.system("hcitool rssi " + list(devices.keys())[0])
+    btcurrent= int(os.popen("echo " + str(cmdout) + " | grep -c \"RSSI return value\" 2> /dev/null").read().replace('RSSI return value: ', '').replace('\n', ''))    
     
-    cmdout= os.popen("hcitool rssi " + list(devices.keys())[0]).read()
-    '''
-    btcurrent= os.popen("echo " + cmdout + " | grep -c \"RSSI return value\") 2> /dev/null").read()
-    rssi= os.popen("echo " + cmdout + " | sed -e 's/RSSI return value: //g')").read()                                   
+    rssi= os.system("echo " + str(cmdout) + " | sed -e 's/RSSI return value: //g'")                                  
 
 
-    if (btcurrent == notconnected ):
-        print(subprocess.Popen("echo Attempting connection...", shell=True, stdout=subprocess.PIPE).stdout.read())
-        print(subprocess.Popen("rfcomm connect 0 $device 1 2> /dev/null >/dev/null &", shell=True, stdout=subprocess.PIPE).stdout.read())
-        print(subprocess.Popen("sleep 1", shell=True, stdout=subprocess.PIPE).stdout.read())        
-        
+    if (btcurrent == notconnected):
+        os.system("echo Attempting connection...")
+        os.system("rfcomm connect 0 " + list(devices.keys())[0] + " 1 2> /dev/null >/dev/null &")
+        os.system("sleep 1")        
+        '''
     if (btcurrent == connected):
-        print(subprocess.Popen("echo \"Device connected. RSSI: \"$rssi", shell=True, stdout=subprocess.PIPE).stdout.read())
+        os.system("echo \"Device connected. RSSI: \"" + str(rssi))
 
     if (btconnected != btcurrent):
         if (btcurrent != 0 ):
-                print(subprocess.Popen("echo GONE!", shell=True, stdout=subprocess.PIPE).stdout.read())
+            os.system("echo GONE!")
         if (btcurrent != 1 ):
-                print(subprocess.Popen("echo HERE!", shell=True, stdout=subprocess.PIPE).stdout.read())
+            os.system("echo HERE!")
 
         btconnected = btcurrent
+'''
 
+os.system("sleep 1")
 
-print(subprocess.Popen("sleep 1", shell=True, stdout=subprocess.PIPE).stdout.read())
+os.system("done")
 
-print(subprocess.Popen("done", shell=True, stdout=subprocess.PIPE).stdout.read())
-''                              
